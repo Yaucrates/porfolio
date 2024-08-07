24b9549517fa14546058734eb7564c244b1869fc
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-08-07 15:03:13 -0400

    Sidebar nav links added. Page placeholders added.

--- src/lib/components/Navbar/NavBody.svelte
+++ src/lib/components/Navbar/NavBody.svelte
@@ -1,8 +1,8 @@
 <div class="w-full p-7 h-64 border-y-2 border-neutral-800 flex flex-col justify-between">
     <div class="flex flex-col gap-2">
         <h2 class="text-sm text-neutral-500">PERSONAL WORK</h2>
-        <a href="/" class="text-sm text-neutral-400 hover:text-neutral-500 transition-all duration-300">Courses</a>
-        <a href="/" class="text-sm text-neutral-400 hover:text-neutral-500 transition-all duration-300">Blog</a>
+        <a href="/courses" class="text-sm text-neutral-400 hover:text-neutral-500 transition-all duration-300">Courses</a>
+        <a href="/blog" class="text-sm text-neutral-400 hover:text-neutral-500 transition-all duration-300">Blog</a>
     </div>
 
     <div class="flex flex-col gap-2">
