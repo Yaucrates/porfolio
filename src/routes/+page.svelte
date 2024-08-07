0ca857213c0d362686ba55b1e353e5b380828f9f
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-08-07 00:51:00 -0400

    About Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,11 @@
     // export let data: PageData;
 </script>
 
+<div class="h-screen flex justify-center items-center select-none">
+    <div class="w-3/4 gap-6 flex flex-col justify-center">
+        <h3 class="text-2xl text-teal-400">Hi, my name is</h3>
+        <h1 class="text-7xl font-bold text-neutral-300">Yousif Abdulhussein.</h1>
+        <h2 class="text-7xl font-bold text-neutral-400">I turn vision into action.</h2>
+        <p class="w-2/3 text-lg text-neutral-400">I’m a computer science major passionate about creating exceptional digital experiences. Currently, I’m focused on learning and building software through academic projects and personal initiatives.</p>
+    </div>
+</div>
