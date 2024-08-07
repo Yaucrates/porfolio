870fa1d36f1cfd8528bd91b7c0f480bc698e1061
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-08-07 17:10:04 -0400

    Header and Nav Added

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -1,18 +1,9 @@
 <script>
 	import Logo from "../Logos/Logo.svelte";
-
-    let expanded = false;
+	import Nav from "./Nav.svelte";
 </script>
 
 <div class="md:hidden p-4 flex justify-between items-center">
     <Logo />
-    <button on:click={() => expanded = !expanded } aria-expanded={expanded} type="button" class="group relative h-6 w-6" style="--width:1.75rem;--thickness:0.125rem;--gap:0.4rem;--color:#06b6d4;--duration:300ms">
-        <span class="absolute left-1/2 top-1/2 h-[var(--thickness)] w-[var(--width)] -translate-x-1/2 translate-y-[calc(-150%-var(--gap))] transition-transform duration-[calc(var(--duration)*2/3)] before:absolute before:right-0 before:h-full before:w-full before:rounded-full before:bg-[var(--color)] before:transition-[width] before:delay-[calc(var(--duration)*1/3)] before:duration-[calc(var(--duration)*2/3)] group-aria-expanded:-translate-y-1/2 group-aria-expanded:-rotate-45 group-aria-expanded:delay-[calc(var(--duration)*1/3)] before:group-aria-expanded:w-[60%] before:group-aria-expanded:delay-0"></span>
-        <span class="absolute left-1/2 top-1/2 h-[var(--thickness)] w-[var(--width)] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[var(--color)] transition-transform duration-[calc(var(--duration)*2/3)] group-aria-expanded:rotate-45 group-aria-expanded:delay-[calc(var(--duration)*1/3)]"></span>
-        <span class="absolute left-1/2 top-1/2 h-[var(--thickness)] w-[var(--width)] -translate-x-1/2 translate-y-[calc(50%+var(--gap))] transition-transform duration-[calc(var(--duration)*2/3)] before:absolute before:right-0 before:h-full before:w-[60%] before:rounded-full before:bg-[var(--color)] before:transition-[right] before:delay-[calc(var(--duration)*1/3)] before:duration-[calc(var(--duration)*2/3)] group-aria-expanded:-translate-y-1/2 group-aria-expanded:-rotate-45 group-aria-expanded:delay-[calc(var(--duration)*1/3)] before:group-aria-expanded:right-[40%] before:group-aria-expanded:delay-0"></span>
-    </button>
+    <Nav />
 </div>
-
-<!--
-    <a href="/about" class="text-sm flex gap-2 hover:text-cyan-500 text-neutral-300 transition-all duration-300"><span class="text-cyan-500">1.</span>About</a>
--->
