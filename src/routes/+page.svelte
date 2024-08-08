5c3272e4123aaaa71aaddb3937d7e8053b08ef5c
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-08-08 16:08:09 -0400

    Nav and landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
     <div class="h-screen flex justify-center items-center select-none">
         <div class="w-3/4 gap-6 flex flex-col justify-center">
             <h3 in:fly={{ y: 200, duration: 1300 }} class="text-[14px] min-[350px]:text-[4vw] sm:text-2xl text-cyan-500">Hi, my name is</h3>
-            <div class="text-[21px] min-[350px]:text-[6vw] sm:text-[5vw] md:text-[4.6vw] font-bold">
+            <div class="text-[21px] min-[350px]:text-[6vw] md:text-[5.4vw] font-bold">
                 <h1 in:fly={{ y: 300, duration: 1200 }} class="text-neutral-300">Yousif Abdulhussein.</h1>
                 <h2 in:fly={{ y: 400, duration: 1100 }} class="text-neutral-400">I turn vision into action.</h2>
             </div>
