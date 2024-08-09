db6e632b33173d272f99d98d07753644ecccaac7
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-08-09 18:20:14 -0500

    about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,4 +4,31 @@
     // export let data: PageData;
 </script>
 
-About Page
+
+
+<div class="mt-20 p-16">
+    <div class="text-5xl text-hero info-hero">
+        I love working on <span class="text-info-hero-serif">cutting edge</span> problems.
+    </div>
+</div>
+
+<style>
+    .text-hero.info-hero {
+        text-align: left;
+        letter-spacing: -1px;
+        font-size: 48px;
+        font-weight: 500;
+        line-height: 110%;
+    }
+
+    .text-info-hero-serif {
+        --on-background: #f2f2f2;
+        
+        background-image: linear-gradient(to bottom, var(--on-background) 50%, #f2f2f233);
+        letter-spacing: -1px;
+        -webkit-text-fill-color: transparent;
+        -webkit-background-clip: text;
+        background-clip: text;
+        font-style: italic;
+    }
+</style>
