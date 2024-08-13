97af9190e0849935610acdc3c271671046e99de2
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-08-13 18:02:46 -0400

    Added Resume

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
 	import ExperienceCard from "$lib/components/ExperienceCard.svelte";
+	import OnMount from "$lib/components/OnMount.svelte";
+	import { fly } from "svelte/transition";
 
     // import type { PageData } from './$types';
     // export let data: PageData;
 </script>
 
-<div class="w-full py-32 gap-32 flex flex-col justify-center items-center">
-    <ExperienceCard background_src="tva-work.webp" logo_src="TennesseeValleyAuthorityLogo.svg" header="May 2024 - Current | Database Engineer Intern" title="Powerline and Transmission Data" description="Focused on creating ways of retrieving and easily accessing data for power lines and transmission." />
-    <ExperienceCard background_src="utk.webp" logo_src="UTK-Logo.png" header="August 2023 - May 2024 | Teacher's Assistant" title="CS Teacher's Assistant" description="Provided support to students with their code by assisting in generating ideas, helping debug code, and clarifying concepts." />
-</div>
+
+<OnMount>
+    <div in:fly={{ y: 200, duration: 1300 }}>
+        <p class="w-full gap-1  py-16 flex justify-center items-center text-lg">
+            Checkout my resume <a href="https://drive.google.com/file/d/1N-R5zF4zIZY0U26gKk62BidB75kiD7YS/view?usp=sharing" target="_blank" class="text-cyan-500 underline underline-offset-2">here.</a>
+        </p>
+        <div class="w-full pb-32 gap-32 flex flex-col justify-center items-center">
+            <ExperienceCard background_src="tva-work.webp" logo_src="TennesseeValleyAuthorityLogo.svg" header="May 2024 - Current | Database Engineer Intern" title="Powerline and Transmission Data" description="Focused on creating ways of retrieving and easily accessing data for power lines and transmission." />
+            <ExperienceCard background_src="utk.webp" logo_src="UTK-Logo.png" header="August 2023 - May 2024 | Teacher's Assistant" title="CS Teacher's Assistant" description="Provided support to students with their code by assisting in generating ideas, helping debug code, and clarifying concepts." />
+        </div>
+    </div>
+</OnMount>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
     // import type { PageData } from './$types';
     import ProjectCard from "$lib/components/ProjectCard.svelte";
+    import OnMount from "$lib/components/OnMount.svelte"
+    import { fly } from "svelte/transition"
     
     // export let data: PageData;
 </script>
 
-<div class="py-20 gap-12 h-full flex flex-col justify-center items-center">
-    <ProjectCard title="Personal Portfolio" subtitle="August 24'" description="Designed my personal portfolio to help showcase what I'm working on!" src="Personal-Portfolio.webp" />
-    <ProjectCard title="Dorm Dojo" subtitle="E-Commerce Project -- May 24'" description="An E-Commerce site that hooked into the shopify API to make a headless storefront." src="DormDojoHomepage.webp" />
-</div>
+<OnMount>
+    <div in:fly={{ y: 200, duration: 1300 }} class="py-10 gap-10 h-full flex flex-col justify-center items-center">
+        <p class="text-lg">
+            Checkout my resume <a href="https://drive.google.com/file/d/1N-R5zF4zIZY0U26gKk62BidB75kiD7YS/view?usp=sharing" target="_blank" class="text-cyan-500 underline underline-offset-2">here</a>.
+        </p>
+        <ProjectCard title="Personal Portfolio" subtitle="August 24'" description="Designed my personal portfolio to help showcase what I'm working on!" src="Personal-Portfolio.webp" />
+        <ProjectCard title="Dorm Dojo" subtitle="E-Commerce Project -- May 24'" description="An E-Commerce site that hooked into the shopify API to make a headless storefront." src="DormDojoHomepage.webp" />
+    </div>
+</OnMount>
