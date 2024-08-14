f5f5405df7d6097856817de5861f03faf97a987e
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-08-13 22:55:24 -0400

    static adapter added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    export const prerender = true;
+
     // import type { LayoutData } from './$types';
     import '../app.css'
     import { onNavigate } from '$app/navigation';
