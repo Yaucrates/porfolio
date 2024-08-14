2885ee3cb2b145c7c2e21d31a5ce1d92981333bb
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-08-13 21:25:16 -0400

    Nav closes on nav

--- src/lib/components/Header/Nav.svelte
+++ src/lib/components/Header/Nav.svelte
@@ -13,10 +13,10 @@
 <nav class={`transitionHeader ${expanded ? "opacity-100 z-20 expanded" : "opacity-0 z-[-1]"} h-full ${expanded ? 'w-full' : 'w-0'} bg-black/10 backdrop-blur-sm absolute top-0 right-0 transition-all duration-300`}>
     <div class={`${expanded ? "w-full min-[384px]:w-96" : "w-0"} h-full absolute flex items-center justify-center top-0 right-0 bg-neutral-900 shadow-md shadow-neutral-900 transition-all duration-300`}>
         <ul class="text-lg min-[384px]:mr-24 flex flex-col gap-4 text-neutral-300 transition-all">
-            <li><a href="/about" class="flex gap-2 hover:text-cyan-500 duration-300"><span class="text-cyan-500">1.</span>About</a></li>
-            <li><a href="/experience" class="flex gap-2 hover:text-cyan-500 duration-300"><span class="text-cyan-500">2.</span>Experience</a></li>
-            <li><a href="/projects" class="flex gap-2 hover:text-cyan-500 duration-300"><span class="text-cyan-500">3.</span>Projects</a></li>
-            <li><a href="/contact" class="flex gap-2 hover:text-cyan-500 duration-300"><span class="text-cyan-500">4.</span>Contact</a></li>
+            <li><a on:click={toggle} href="/about" class="flex gap-2 hover:text-cyan-500 duration-300"><span class="text-cyan-500">1.</span>About</a></li>
+            <li><a on:click={toggle} href="/experience" class="flex gap-2 hover:text-cyan-500 duration-300"><span class="text-cyan-500">2.</span>Experience</a></li>
+            <li><a on:click={toggle} href="/projects" class="flex gap-2 hover:text-cyan-500 duration-300"><span class="text-cyan-500">3.</span>Projects</a></li>
+            <li><a on:click={toggle} href="/contact" class="flex gap-2 hover:text-cyan-500 duration-300"><span class="text-cyan-500">4.</span>Contact</a></li>
         </ul>
     </div>
 </nav>
