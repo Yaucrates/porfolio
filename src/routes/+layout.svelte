3f4c9a1dd09b4c06149f79fbdc1243bb1dbdfc0f
Yaucrates <abdulhusseinyousifh@gmail.com>
2025-01-03 22:35:14 -0500

    sv reset

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,63 +1,6 @@
 <script lang="ts">
-    // import type { LayoutData } from './$types';
-    import '../app.css'
-    import { onNavigate } from '$app/navigation';
-	import SideNav from "$lib/components/Navigation/SideNav.svelte";
-	import Header from '$lib/components/Navigation/Header.svelte';
-	import Footer from '$lib/components/Navigation/Footer.svelte';
-    
-    onNavigate((navigation) => {
-        if (!document.startViewTransition) return;
-	    return new Promise((resolve) => {
-            document.startViewTransition(async () => {
-                resolve();
-                await navigation.complete;
-            });
-        });
-    });
-    
-    // export let data: LayoutData;
+	import '../app.css';
+	let { children } = $props();
 </script>
 
-<SideNav />
-<div class="md:ml-48 h-full flex flex-col">
-    <Header />
-    <div class="flex-grow">
-        <slot />
-        <Footer />
-    </div>
-</div>
-
-<style>
-    @keyframes fade-in {
-        from {
-            opacity: 0;
-        }
-    }
-
-    @keyframes fade-out {
-        to {
-            opacity: 0;
-        }
-    }
-
-    @keyframes slide-from-top {
-        from {
-            transform: translateY(30px);
-        }
-    }
-
-    @keyframes slide-to-bottom {
-        to {
-            transform: translateY(-30px);
-        }
-    }
-
-    :root::view-transition-old(root) {
-        animation: 90ms cubic-bezier(0.4, 0, 1, 1) both fade-out, 300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-to-bottom;
-    }
-
-    :root::view-transition-new(root) {
-        animation: 210ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in, 300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-top;
-    }
-</style>
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,2 @@
-<script lang="ts">
-    // import type { PageData } from './$types';
-    import OnMount from "$lib/components/OnMount.svelte";
-    import { fly } from "svelte/transition"
-    
-    // export let data: PageData;
-</script>
-
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Homepage for Yousif Abdulhussein's Personal Site." />
-</svelte:head>
-
-<OnMount>
-    <div class="h-full flex justify-center items-center">
-        <div class="w-4/5 gap-6 flex flex-col justify-center">
-            <h3 in:fly={{ y: 200, duration: 1300 }} class="text-[14px] min-[350px]:text-[4vw] sm:text-2xl text-cyan-500">Hi, my name is</h3>
-            <div class="text-[21px] min-[350px]:text-[6vw] md:text-[5.4vw] font-bold">
-                <h1 in:fly={{ y: 400, duration: 1100 }} class="text-neutral-300">Yousif Abdulhussein.</h1>
-                <h2 in:fly={{ y: 600, duration: 900 }} class="text-neutral-400">I turn vision into action.</h2>
-            </div>
-            <p in:fly={{ y: 800, duration: 700 }} class="sm:w-2/3 text-base sm:text-lg text-neutral-400">I’m a computer science major passionate about creating exceptional digital experiences. Currently, I’m focused on learning and building software through academic projects and personal initiatives.</p>
-        </div>
-    </div>
-    
-</OnMount>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
