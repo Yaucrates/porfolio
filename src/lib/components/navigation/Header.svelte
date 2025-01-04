2739bb8f222e1eef04f50dfbe130472ae5d810ed
Yaucrates <abdulhusseinyousifh@gmail.com>
2025-01-03 23:22:36 -0500

    header

--- src/lib/components/navigation/Header.svelte
+++ src/lib/components/navigation/Header.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Logo from "$lib/components/logos/Logo.svelte";
-	import MobileNav from "./MobileNav.svelte";
+	import MobileNav from "$lib/components/navigation/MobileNav.svelte";
 </script>
 
 <div class="md:hidden p-4 flex justify-between items-center z-max">
