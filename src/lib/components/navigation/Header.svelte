c1eaa93d2db91c47a9d810419d61d7da7371921e
Yaucrates <abdulhusseinyousifh@gmail.com>
2025-01-03 23:00:23 -0500

    logo path fixed

--- src/lib/components/navigation/Header.svelte
+++ src/lib/components/navigation/Header.svelte
@@ -1,9 +1,9 @@
 <script>
-	import Logo from "../Logos/Logo.svelte";
-	import MobileNav from "./MobileNav.svelte";
+	import Logo from "$lib/components/logos/Logo.svelte";
+	// import MobileNav from "./MobileNav.svelte";
 </script>
 
 <div class="md:hidden p-4 flex justify-between items-center z-max">
     <Logo />
-    <MobileNav />
+    <!-- <MobileNav /> -->
 </div>
