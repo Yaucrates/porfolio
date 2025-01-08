5dd6e5725ae85125535ceb1c69f5c10ddf7f906b
Yaucrates <abdulhusseinyousifh@gmail.com>
2025-01-07 19:11:36 -0500

    Wensite -> Website

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -11,7 +11,7 @@
 
 <OnMount>
     <div in:fly={{ y: 200, duration: 1300 }} class="py-16 gap-12 h-full flex flex-col justify-center items-center">
-        <ProjectCard href="https://www.calidojo.com/" title="CaliDojo" subtitle="Jan 1st, 24'" description="Designed a wensite that provides calisthenics information along with an affiliate marketing shopping page." src="/projects/CaliDojo.webp" />
+        <ProjectCard href="https://www.calidojo.com/" title="CaliDojo" subtitle="Jan 1st, 24'" description="Designed a website that provides calisthenics information along with an affiliate marketing shopping page." src="/projects/CaliDojo.webp" />
         <ProjectCard href="https://www.ceptionlabs.com/" title="Ception Labs" subtitle="December 24'" description="Designed a web development agency!" src="/projects/CeptionLabs.webp" />
         <ProjectCard href="https://www.yousifabdulhussein.com/" title="Personal Portfolio" subtitle="August 24'" description="Designed my personal portfolio to help showcase what I'm working on!" src="/projects/Personal-Portfolio.webp" />
         <ProjectCard href="https://www.dormdojo.com/" title="Dorm Dojo" subtitle="E-Commerce Project -- May 24'" description="An E-Commerce site that hooked into the shopify API to make a headless storefront." src="/projects/DormDojoHomepage.webp" />
