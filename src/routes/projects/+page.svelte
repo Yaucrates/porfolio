ac68cd0949511483cd779403492ad766ae7e5e1b
Yaucrates <abdulhusseinyousifh@gmail.com>
2025-01-30 10:37:53 -0500

    updated

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -11,6 +11,7 @@
 
 <OnMount>
     <div in:fly={{ y: 200, duration: 1300 }} class="py-16 gap-12 h-full flex flex-col justify-center items-center">
+        <ProjectCard href="https://www.imparteducation.com/" title="Impart Education" subtitle="Web Project -- January 25'" description="Designing a Virtual School to allow students to learn advanced college courses." src="/projects/impart-education-new.webp" />
         <ProjectCard href="https://www.calidojo.com/" title="CaliDojo" subtitle="Jan 1st, 24'" description="Designed a website that provides calisthenics information along with an affiliate marketing shopping page." src="/projects/CaliDojo.webp" />
         <ProjectCard href="https://www.ceptionlabs.com/" title="Ception Labs" subtitle="December 24'" description="Designed a web development agency!" src="/projects/CeptionLabs.webp" />
         <ProjectCard href="https://www.yousifabdulhussein.com/" title="Personal Portfolio" subtitle="August 24'" description="Designed my personal portfolio to help showcase what I'm working on!" src="/projects/Personal-Portfolio.webp" />
