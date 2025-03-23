70b1f625cb06e3725372b55c089c444e24791f76
Yaucrates <abdulhusseinyousifh@gmail.com>
2025-03-22 23:25:12 -0400

    Removed Insta, Twitter, and Youtube

--- src/lib/components/navigation/block/Socials.svelte
+++ src/lib/components/navigation/block/Socials.svelte
@@ -2,7 +2,4 @@
     <h2 class="text-sm text-neutral-500">SOCIALS</h2>
     <a href="https://www.linkedin.com/in/yousif-abdulhussein-23400a203/" target="_blank" class="w-fit text-sm text-neutral-400 hover:text-neutral-500 transition-all duration-300">LinkedIn</a>
     <a href="https://github.com/Yaucrates" target="_blank" class="w-fit text-sm text-neutral-400 hover:text-neutral-500 transition-all duration-300">GitHub</a>
-    <a href="https://www.youtube.com/@yousifabdulhussein" target="_blank" class="w-fit text-sm text-neutral-400 hover:text-neutral-500 transition-all duration-300">YouTube</a>
-    <a href="https://www.instagram.com/yousifeeni/?hl=en" target="_blank" class="w-fit text-sm text-neutral-400 hover:text-neutral-500 transition-all duration-300">Instagram</a>
-    <a href="https://x.com/yousifeeni" target="_blank" class="w-fit text-sm text-neutral-400 hover:text-neutral-500 transition-all duration-300">Twitter</a>
 </div>
