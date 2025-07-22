34dc80bc4685438710d1139e23852e8ca2c016e5
Yaucrates <abdulhusseinyousifh@gmail.com>
2025-07-22 16:39:32 -0400

    Making Education Accessible

--- src/lib/components/navigation/block/Personal-Work.svelte
+++ src/lib/components/navigation/block/Personal-Work.svelte
@@ -1,5 +1,4 @@
 <div class="w-fit flex flex-col gap-2">
     <h2 class="text-sm text-neutral-500">PERSONAL WORK</h2>
-    <a class="w-fit text-sm text-neutral-400 hover:text-neutral-500 transition-all duration-300" target="_blank" href="https://www.imparteducation.com/">Education</a>
-    <a class="w-fit text-sm text-neutral-400 hover:text-neutral-500 transition-all duration-300" target="_blank" href="https://www.ceptionlabs.com/">Agency</a>
+    <a class="w-fit text-sm text-neutral-400 hover:text-neutral-500 transition-all duration-300" target="_blank" href="https://www.imparteducation.com/">Making Education Accessible</a>
 </div>
