9da0ad31504318f03fa24ce8e3b9ab89eebbe2ee
Yousif Abdulhussein <abdulhusseinyousifh@gmail.com>
2025-12-18 00:10:17 -0500

    Impart Education Update

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -11,7 +11,7 @@
 
 <OnMount>
     <div in:fly={{ y: 200, duration: 1300 }} class="w-full py-32 gap-32 flex flex-col justify-center items-center">
-        <ExperienceCard background_src="/experience-images/ImpartEducation.png" logo_src="/experience-images/ImpartLogo.svg" header="May 2025 - Current | Impart Education" title="Full Stack Engineer" description="Spearheaded development of a scalable web-based education platform. Built and maintained end-to-end features using Sveltekit on the frontend and Node.js on the backend." />
+        <ExperienceCard background_src="/experience-images/ImpartEducation.png" logo_src="/experience-images/ImpartLogo.svg" header="May 2025 - June 2025 | Impart Education" title="Full Stack Engineer" description="Spearheaded development of a scalable web-based education platform. Built and maintained end-to-end features using Sveltekit on the frontend and Node.js on the backend." />
         <ExperienceCard background_src="/experience-images/tva-work.webp" logo_src="/experience-images/TennesseeValleyAuthorityLogo.svg" header="May 2024 - May 2025 | Tennessee Valley Authority" title="Data Engineering Intern" description="Focused on creating ways of retrieving and easily accessing data for power lines and transmission." />
         <ExperienceCard background_src="/experience-images/utk.webp" logo_src="/experience-images/UTK-Logo.png" header="August 2023 - Current | University of Tennessee Knoxville" title="CS Teacher's Assistant" description="Provided support to students with their code by assisting in generating ideas, helping debug code, and clarifying concepts for Introduction to Computer Science & Data Structures and Algorithms." />
     </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -11,7 +11,7 @@
 
 <OnMount>
     <div in:fly={{ y: 200, duration: 1300 }} class="py-16 gap-12 h-full flex flex-col justify-center items-center">
-        <ProjectCard href="https://www.imparteducation.com/" title="Impart Education" subtitle="Web Project -- January 25'" description="Designing a Virtual School to allow students to learn advanced college courses." src="/projects/impart-education-new.webp" />
+        <ProjectCard href="https://www.imparteducation.com/" title="Impart Education" subtitle="Web Project -- May 25'" description="Designing a Virtual School to allow students to learn advanced college courses." src="/projects/impart-education-new.webp" />
         <ProjectCard href="https://cali-dojo.vercel.app/" title="CaliDojo" subtitle="Jan 1st, 24'" description="Designed a website that provides calisthenics information along with an affiliate marketing shopping page." src="/projects/CaliDojo.webp" />
         <ProjectCard href="https://ceptionlabs.vercel.app/" title="Ception Labs" subtitle="December 24'" description="My own web development agency which I did some projects under!" src="/projects/CeptionLabs.webp" />
         <ProjectCard href="https://www.yousifabdulhussein.com/" title="Personal Portfolio" subtitle="August 24'" description="Designed my personal portfolio to help showcase what I'm working on!" src="/projects/Personal-Portfolio.webp" />
